26f816677581182675f6e1365a2dbf2c6523f21f
Dylan Wilson <dwilson5817@gmail.com>
2025-12-31 19:47:45 +0000

    feat(cdk): add S3 buckets for backups and archive storage
    Create two S3 buckets: one for archive storage and one for daily
    backups. Configure lifecycle rule to transition backup data to Deep
    Archive after 3 days.

--- lib/infrastructure-backups-cdk-stack.ts
+++ lib/infrastructure-backups-cdk-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class InfrastructureBackupsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'ArchiveBucket', {
+      bucketName: 'dylanw.net-archive',
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureBackupsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'DailyBackupsBucket', {
+      bucketName: 'london.dylanw.net-backups',
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      lifecycleRules: [
+        {
+          id: 'TransitionToDeepArchive',
+          enabled: true,
+          transitions: [
+            {
+              storageClass: s3.StorageClass.DEEP_ARCHIVE,
+              transitionAfter: cdk.Duration.days(3),
+            },
+          ],
+        },
+      ],
+    });
   }
 }
