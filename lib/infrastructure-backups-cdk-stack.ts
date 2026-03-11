290960675865e83598bf1795b7ad11b39c3bc26f
Dylan Wilson <dwilson5817@gmail.com>
2026-03-11 23:20:41 +0000

    feat(cdk): add per-host S3 buckets and IAM roles for backups
    Generate S3 buckets and IAM roles specific to each host and guest VM.
    Configure lifecycle rules for transitioning backups to Deep Archive and
    set appropriate permissions for Roles Anywhere access.

--- lib/infrastructure-backups-cdk-stack.ts
+++ lib/infrastructure-backups-cdk-stack.ts
@@ -1,6 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+
+interface HostToGuests {
+  [host: string]: string[]
+}
+
+const hostsToBackup: HostToGuests = {
+  'london.dylanw.net': ['git01', 'sql01', 'web01', 'game01', 'game02']
+};
 
 export class InfrastructureBackupsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,21 +20,47 @@ export class InfrastructureBackupsCdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.RETAIN,
     });
 
-    new s3.Bucket(this, 'DailyBackupsBucket', {
-      bucketName: 'london.dylanw.net-backups',
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
-      lifecycleRules: [
-        {
-          id: 'TransitionToDeepArchive',
-          enabled: true,
-          transitions: [
-            {
-              storageClass: s3.StorageClass.DEEP_ARCHIVE,
-              transitionAfter: cdk.Duration.days(3),
+    Object.keys(hostsToBackup).forEach(host => hostsToBackup[host].forEach(vm => {
+      const hostname = `${vm}.${host}`;
+
+      const role = new iam.Role(this, `BackupRole-${hostname}`, {
+        assumedBy: new iam.ServicePrincipal('rolesanywhere.amazonaws.com'),
+        roleName: `BackupRole-${hostname}`,
+        description: `Backup role for ${hostname}`,
+        externalIds: [],
+      });
+
+      role.assumeRolePolicy?.addStatements(
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            principals: [new iam.ServicePrincipal('rolesanywhere.amazonaws.com')],
+            actions: ['sts:AssumeRole', 'sts:TagSession', 'sts:SetSourceIdentity'],
+            conditions: {
+              StringEquals: {
+                'aws:PrincipalTag/x509Subject/CN': hostname,
+              },
             },
-          ],
-        },
-      ],
-    });
+          })
+      );
+
+      const backupsBucket = new s3.Bucket(this, `BackupsBucket-${hostname}`, {
+        bucketName: `${hostname}-backups`,
+        removalPolicy: cdk.RemovalPolicy.RETAIN,
+        lifecycleRules: [
+          {
+            id: 'TransitionToDeepArchive',
+            enabled: true,
+            transitions: [
+              {
+                storageClass: s3.StorageClass.DEEP_ARCHIVE,
+                transitionAfter: cdk.Duration.days(3),
+              },
+            ],
+          },
+        ],
+      });
+
+      backupsBucket.grantReadWrite(role);
+    }));
   }
 }
