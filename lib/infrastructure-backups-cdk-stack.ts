e9af6a51c06343fe7694b2fcd8dea433e2eab960
Dylan Wilson <dwilson5817@gmail.com>
2026-03-12 00:28:22 +0000

    feat(cdk): add Roles Anywhere profiles for host-specific IAM roles
    Create a Roles Anywhere profile bound to the role for each host.

--- lib/infrastructure-backups-cdk-stack.ts
+++ lib/infrastructure-backups-cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as rolesanywhere from 'aws-cdk-lib/aws-rolesanywhere';
 
 interface HostToGuests {
   [host: string]: string[]
@@ -61,6 +62,11 @@ export class InfrastructureBackupsCdkStack extends cdk.Stack {
       });
 
       backupsBucket.grantReadWrite(role);
+
+      new rolesanywhere.CfnProfile(this, `RoleAnywhereProfile-${hostname}`, {
+        name: hostname,
+        roleArns: [ role.roleArn ],
+      });
     }));
   }
 }
