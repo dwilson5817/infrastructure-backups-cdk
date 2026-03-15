3398382e0c5e56eeeaf5aa295213c03fd4ecfcc5
Dylan Wilson <dwilson5817@gmail.com>
2026-03-15 01:34:25 +0000

    refactor(cdk): comment out all backup hosts in `backups-hosts.ts`
    Will manually add these back in one-by-one as they are ready to start
    pushing backups to S3.

--- lib/config/backups-hosts.ts
+++ lib/config/backups-hosts.ts
@@ -1,5 +1,12 @@
 export type HostGroupMap = Record<string, string[]>;
 
 export const hostsToBackup: HostGroupMap = {
-    'london.dylanw.net': ['git01', 'sql01', 'web01', 'mail01', 'game01', 'game02'],
+    'london.dylanw.net': [
+        // 'game01',
+        // 'game02',
+        // 'git01',
+        // 'mail01',
+        // 'sql01',
+        // 'web01',
+    ],
 };
